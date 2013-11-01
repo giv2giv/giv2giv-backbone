a4cbb452714d22139bc2f3dec04b71ca2e32c4f3
Fitria Rahmaani <fitria@41studio.com>
2013-11-01 20:49:22 +0700

    implemented create endowment in wizard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,15 +1,22 @@
 class DashboardController < ApplicationController
   def index
+    session[:button] = "dashboard"
+  end
+
+  def new_endowment
+    session[:button] = "new_endowment"
     unless session[:user_signed_in]
       redirect_to sign_in_path
     end
   end
 
-  def new_endowment
-  end
-
   def create_endowment
-    response = RestClient.post "#{SERVER_URL}api/endowment.json", { token: session[:token], name: params[:name], minimum_donation_amount: params[:minimum_donation_amount], endowment_visibility: params[:endowment_visibility] }, content_type: :json, accept: :json
-    session[:endowment] = JSON.parse(response.body)
+    params_endowment = { name: params[:name], minimum_donation_amount: params[:minimum_donation_amount], endowment_visibility: params[:endowment_visibility] }
+    request = Typhoeus::Request.new("#{SERVER_URL}api/endowment.json", method: :post, params: params_endowment, headers: { 'Content-Type'=> "application/json", 'Authorization' => "Token token=#{session[:token]}" })
+    response = request.run
+
+    if response.code == 201
+      session[:endowment] = JSON.parse(response.body)["endowment"]
+    end
   end
 end
